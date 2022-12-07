cb98c8f4dfb086f389bf306451535cfe73624943
DanWaLes <daniel.church@btinternet.com>
2022-12-07 20:09:01 +0000

    empty array/object visual fix

--- 2022/day7/shared.js
+++ 2022/day7/shared.js
@@ -1,65 +1,6 @@
 (() => {
 	const fileUtil = require('./fileUtil');
 
-	function readFs(line, fs, cd, inLs) {
-		if (!fs) {
-			fs = {'/': {dirs: [], files: []}};
-		}
-
-		const cdCmd = line.match(/^\$ cd (.+)$/);
-		const lsCmd = line.match(/^\$ ls$/);
-
-		if (cdCmd) {
-			inLs = false;
-			const dirName = cdCmd[1];
-
-			if (dirName == '..') {
-				cd = cd.replace(/[^\/]+\/$/, '');
-			}
-			else if (dirName == '/') {
-				cd = '/';
-			}
-			else {
-				cd += dirName + '/';
-			}
-		}
-		else if (lsCmd) {
-			inLs = true;
-		}
-		else if (inLs) {
-			const newDir = line.match(/^dir (.+)/);
-			const newFile = line.match(/^(\d+) (.+)/);
-
-			if (newDir) {
-				const newDirName = newDir[1];
-				const newDirLocation = cd + newDirName + '/';
-
-				fs[cd].dirs.push(newDirLocation);
-				fs[newDirLocation] = {dirs: [], files: []};
-			}
-			else if (newFile) {
-				fs[cd].files.push({name: newFile[2], size: parseInt(newFile[1])});
-			}
-			else {
-				throw 'this should never happen';
-			}
-		}
-
-		return {fs, cd, inLs};
-	}
-
-	function calcDirSize(fs, dir, currSum) {
-		for (let file of fs[dir].files) { 
-			currSum += file.size;
-		}
-
-		for (let newDir of fs[dir].dirs) {
-			currSum += calcDirSize(fs, newDir, 0);
-		}
-
-		return currSum;
-	}
-	
 	function prettyPrint(obj) {
 		let indent = '';
 		let str = '';
@@ -86,7 +27,11 @@
 					}
 				}
 
-				str += '\n' + indent + ']';
+				if (obj.length) {
+					str += '\n' + indent;
+				}
+
+				str += ']';
 			}
 			else if (obj && typeof obj == 'object') {
 				str += '{';
@@ -107,7 +52,12 @@
 				}
 
 				indent = indent.replace(/^\t/, '');
-				str += '\n' + indent + '}';
+
+				if (keys.length) {
+					str += '\n' + indent;
+				}
+
+				str += '}';
 			}
 			else {
 				str += JSON.stringify(obj);
@@ -119,5 +69,5 @@
 		return str;
 	}
 
-	module.exports = {fileUtil, readFs, calcDirSize, prettyPrint};
+	module.exports = {fileUtil, prettyPrint};
 })();
